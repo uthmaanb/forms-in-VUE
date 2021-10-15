fe272f1e03b917b0d3fada83e3784b36759f727f
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-15 08:54:36 +0200

    submitting forms

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -20,6 +21,10 @@
 
     <input type="checkbox" required v-model="terms" />
     <label>Accept terms and conditions</label>
+
+    <div class="submit">
+      <button>create an account</button>
+    </div>
 
     <input type="checkbox" value="shaun" v-model="names" />
     <label>shaun</label>
@@ -46,6 +51,7 @@ export default {
       names: [],
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -61,6 +67,21 @@ export default {
       this.skills = this.skills.filter((item) => {
         return skill !== item;
       });
+    },
+    handleSubmit() {
+      //  valdate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password needs to be at least 6 characters long";
+
+      if (!this.passwordError) {
+        console.log("email: ", this.email);
+        console.log("password: ", this.password);
+        console.log("role: ", this.role);
+        console.log("skills: ", this.skills);
+        console.log("terms accepted: ", this.terms);
+      }
     },
   },
 };
@@ -112,5 +133,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
